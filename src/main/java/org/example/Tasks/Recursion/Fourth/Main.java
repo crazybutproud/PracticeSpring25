b106247ac43aaf369c10bc29a6036fb892c2f5d9
crazybutproud <ms.terebonk@mail.ru>
2025-04-23 22:01:06 +0300

    The solution to the problem with the Ackerman function has been corrected

--- src/main/java/org/example/Tasks/Recursion/Fourth/Main.java
+++ src/main/java/org/example/Tasks/Recursion/Fourth/Main.java
@@ -1,7 +1,7 @@
 package org.example.Tasks.Recursion.Fourth;
 //Функция Аккермана
 //В теории вычислимости важную роль играет функция Аккермана A(m,n)
-//Даны два целых неотрицательных числа m и n, каждое в отдельной строке. Выведите A(m,n)
+//Даны два целых неотрицательных числа m и n, каждое в отдельной строке. Выведите A(m,n).
 public class Main {
     public static void main(String[] args) {
         System.out.println(recursionA(2,6));
